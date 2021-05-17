adeecfa8c3ed969f2c47cf26f92ff890e904d082
Jonas Temple <jtemple@themagnetgroup.com>
2021-05-17 15:42:43 -0500

    Added indication if product is mfg or kit bom.

--- tmg_accounting/models/models.py
+++ tmg_accounting/models/models.py
@@ -25,18 +25,30 @@ class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
     cost_total = fields.Float('Cost Total', readonly=True)
+    bom_type = fields.Selection([
+        ('normal', 'Manufacture'),
+        ('phantom', 'Kit')
+        ], string='BoM Type', readonly=True)
 
     def _select(self):
         select_str = super(AccountInvoiceReport, self)._select()
         select_str += ', sub.cost_total'
+        select_str += ', sub.bom_type'
         return select_str
 
     def _sub_select(self):
         select_str = super(AccountInvoiceReport, self)._sub_select()
         select_str += ', SUM(ail.quantity * irp.value_float) AS cost_total'
+        select_str += ', bom.type as bom_type'
         return select_str
 
     def _from(self):
         from_str = super(AccountInvoiceReport, self)._from()
         from_str += """ left join ir_property irp on concat('product.product,',ail.product_id) = irp.res_id and irp.name='standard_price'"""
+        from_str += ' left join mrp_bom bom on pt.id = bom.product_tmpl_id'
         return from_str
+
+    def _group_by(self):
+        group_by_str = super(AccountInvoiceReport, self)._group_by()
+        group_by_str += ', bom.type'
+        return group_by_str
